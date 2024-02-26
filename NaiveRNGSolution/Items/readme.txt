e2c49693ca1b18ec152dd52f4d719f6e51f5621c
pavelski01 <pavelski01@gmail.com>
2024-02-26 08:26:44 +0100

    init

---
+++ NaiveRNGSolution/Items/readme.txt
@@ -0,0 +1,4 @@
+dotnet tool update -g dotnet-counters
+
+dotnet-counters monitor -n NaiveRNG GeneratorMeter
+dotnet-counters monitor -n NaiveRNG
---
+++ NaiveRNGSolution/NaiveRNG/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static readonly Meter s_meter = new("GeneratorMeter", "1.0.0");
+    static readonly Counter<int> s_numbersGenerated = s_meter.CreateCounter<int>(
+        name: "number-generated",
+        //unit: "numbers",
+        description: "Numbers generated");
+
+    static void Main(string[] args)
+    {
+        using MeterProvider meterProvider = Sdk.CreateMeterProviderBuilder()
+                .AddMeter("GeneratorMeter")
+                .AddPrometheusHttpListener(options => options.UriPrefixes = new string[] { "http://host.docker.internal:9184/" })
+                .AddAspNetCoreInstrumentation()
+                .AddOtlpExporter(opts => opts.Endpoint = new Uri("http://host.docker.internal:4317"))
+                .Build();
+
+        var rand = Random.Shared;
+        Console.WriteLine("Press any key to exit");
+        int number;
+        while (!Console.KeyAvailable)
+        {
+            number = rand.Next(100, 1000);
+            Thread.Sleep(number);
+            s_numbersGenerated.Add(number);
+        }
+    }
+}
