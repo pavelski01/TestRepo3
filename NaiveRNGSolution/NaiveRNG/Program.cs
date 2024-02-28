3a125c73fb86424f26651ae1facd00f49886b0e2
pavelski01 <pavelski01@gmail.com>
2024-02-28 01:09:33 +0100

    Insight

--- NaiveRNGSolution/NaiveRNG/Program.cs
+++ NaiveRNGSolution/NaiveRNG/Program.cs
@@ -1,4 +1,9 @@
-﻿using OpenTelemetry;
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 using System.Diagnostics.Metrics;
@@ -13,6 +18,22 @@ class Program
 
     static void Main(string[] args)
     {
+        using var channel = new InMemoryChannel();
+
+        IServiceCollection services = new ServiceCollection();
+        services.Configure<TelemetryConfiguration>(config => config.TelemetryChannel = channel);
+        services.AddLogging(builder =>
+        {
+            // Only Application Insights is registered as a logger provider
+            builder.AddApplicationInsights(
+                configureTelemetryConfiguration: (config) => config.ConnectionString = "InstrumentationKey=c469665d-0c16-497e-9a55-26971a7e950a;IngestionEndpoint=https://polandcentral-0.in.applicationinsights.azure.com/;LiveEndpoint=https://polandcentral.livediagnostics.monitor.azure.com/",
+                configureApplicationInsightsLoggerOptions: (options) => { }
+            );
+        });
+
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        ILogger<Program> logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
         using MeterProvider meterProvider = Sdk.CreateMeterProviderBuilder()
                 .AddMeter("GeneratorMeter")
                 .AddPrometheusHttpListener(options => options.UriPrefixes = new string[] { "http://host.docker.internal:9184/" })
@@ -28,6 +49,7 @@ class Program
             number = rand.Next(100, 1000);
             Thread.Sleep(number);
             s_numbersGenerated.Add(number);
+            logger.LogInformation("Logger is working...");
         }
     }
 }
